77a0e1dea3686773820c56b68692c9d5ae223727
ElianaFlaxer <flaxereliana@gmail.com>
2022-02-09 11:31:04 +0200

    added artist as a premium user

---
+++ main.py
@@ -0,0 +1,10 @@
+from people.artist import Artist
+
+
+def main():
+
+    lili = Artist()
+
+
+if __name__ == '__main__':
+    main()
--- people/artist.py
+++ people/artist.py
@@ -1,4 +1,7 @@
-class Artist:
+from people.premium import Premium
+
+
+class Artist(Premium):
   def __init__(self, id, name ,albums:list, genre):
     self.id = id
     self.name = name
